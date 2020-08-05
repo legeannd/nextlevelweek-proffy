f515e0419da1ec6dbb02d386fcc093b2f6bfcb64
Gean Lucas <gean.lfpsa@gmail.com>
2020-08-05 15:54:53 -0300

    #server - Classes and connections controllers created with routes

--- server/src/database/migrations/02_create_class_schedule.ts
+++ server/src/database/migrations/02_create_class_schedule.ts
@@ -3,7 +3,7 @@ import Knex from 'knex';
 export async function up(knex: Knex) {
   return knex.schema.createTable('class_schedule', table => {
     table.increments('id').primary();
-    table.integer('weekday').notNullable();
+    table.integer('week_day').notNullable();
     table.integer('from').notNullable();
     table.integer('to').notNullable();
 
