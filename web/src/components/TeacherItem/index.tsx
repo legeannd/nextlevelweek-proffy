e0bf4f55db9d0c54a14e90a0e82df78049f2e9cd
Gean Lucas <gean.lfpsa@gmail.com>
2020-08-05 22:45:19 -0300

    #web - Classes being created in TeacherForm page + TeacherList getting specified teachers

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,55 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem () {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
           <header>
-            <img src="https://avatars0.githubusercontent.com/u/24641532?s=460&u=2fd2df1fae9711cebf45c19d7c10ad8d09ee12b7&v=4" alt="Gean Lucas"/>
+            <img src={teacher.avatar} alt={teacher.name}/>
             <div>
-              <strong>Gean Lucas</strong>
-              <span>Química</span>
+              <strong>{teacher.name}</strong>
+              <span>{teacher.subject}</span>
             </div>
           </header>
 
           <p>
-            Entusiasta das melhores tecnologias de química avançada.
-            <br /><br />
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+            {teacher.bio}
           </p>
 
           <footer>
             <p>
               Preço/hora
-              <strong>R$ 100,00</strong>
+              <strong>R$ {teacher.cost}</strong>
             </p>
-            <button type="button">
+            <a 
+              target="_blank" 
+              onClick={createNewConnection} 
+              href={`https://wa.me/${teacher.whatsapp}`}>
               <img src={whatsappIcon} alt="Whatsapp"/>
               Entrar em contato
-            </button>
+            </a>
           </footer>
         </article>
   )
