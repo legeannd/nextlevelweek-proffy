7b798e2bd9f622a16b39edba54fa2a9f1c94bc2f
Gean Lucas <gean.lfpsa@gmail.com>
2020-08-08 16:09:26 -0300

    #mobile - Toggle favorite teacher on TeacherList page finished

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { View, ScrollView, Text, TextInput } from 'react-native';
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-community/async-storage';
+
 import api from '../../services/api';
 
 import PageHeader from '../../components/PageHeader';
@@ -12,15 +14,32 @@ import styles from './styles';
 function TeacherList() {
   const [isFiltersVisible, setIsFiltersVisible] = useState(false);
   const [teachers, setTeachers] = useState([]);
+  const [favorites, setFavorites] = useState<Number[]>([]);
+
   const [subject, setSubject] = useState('');
   const [week_day, setWeekDay] = useState('');
   const [time, setTime] = useState('');
+
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+        const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher) => {
+          return teacher.id;
+        })
+
+        setFavorites(favoritedTeachersIds);
+      }
+    });
+  }
 
   function handleToggleFiltersVisible() {
     setIsFiltersVisible(!isFiltersVisible);
   }
 
   async function handleFiltersSubmit() {
+    loadFavorites();
+
     const response = await api.get('classes', {
       params: {
         subject,
@@ -91,9 +110,15 @@ function TeacherList() {
           paddingBottom: 16,
         }}
       >
-        {teachers.map((teacher: Teacher)=> (
-          <TeacherItem key={teacher.id} teacher={teacher} />
-        ))}
+        {teachers.map((teacher: Teacher)=> {
+          return (
+            <TeacherItem 
+              key={teacher.id} 
+              teacher={teacher} 
+              favorited={favorites.includes(teacher.id)}
+            />
+          )
+        })}
       </ScrollView>
     </View>
   );
